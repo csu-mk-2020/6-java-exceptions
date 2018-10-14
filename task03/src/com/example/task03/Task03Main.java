f0a414f15c065cb2a2dd0c45c6d846f12df3d40a
Alexandr Dolgov <aydolgov@gmail.com>
2018-10-14 21:39:16 +0500

    Задание 03 Кидаем checked исключение

---
+++ task03/src/com/example/task03/Task03Main.java
@@ -0,0 +1,14 @@
+package com.example.task03;
+
+public class Task03Main {
+    public static void main(String[] args) {
+        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
+        // например вот так:
+        /*
+        throwCheckedException();
+         */
+    }
+
+    //todo напишите здесь свою корректную реализацию задания
+
+}
