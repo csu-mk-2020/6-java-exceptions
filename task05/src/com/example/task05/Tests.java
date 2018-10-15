907f8913ef68b9123166d002c07ab121bce7afc0
alexd <alexd@z-inc.net>
2018-10-15 12:51:15 +0500

    Задание 05 Catch и ещё раз catch

---
+++ task05/src/com/example/task05/Tests.java
@@ -0,0 +1,61 @@
+package com.example.task05;
+
+import codecheck.CodeParser;
+import com.github.javaparser.ast.NodeList;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.stmt.CatchClause;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.List;
+
+public class Tests {
+
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+    private final PrintStream originalSystemOut = System.out;
+
+    @Before
+    public void setUpSystemOut() {
+        System.setOut(new PrintStream(out));
+    }
+
+    @After
+    public void cleanUpSystemOut() {
+        System.setOut(originalSystemOut);
+    }
+
+    @Test
+    public void testCatchFileNotFound() {
+        String[] args = {"abcd"};
+        Task05Main.main(args);
+        String actual = out.toString();
+        Assert.assertEquals("файл \"abcd\" не найден\n", actual);
+    }
+
+    @Test
+    public void testMainNotDeclareThrows() throws Exception {
+        MethodDeclaration method = new CodeParser("task05", Task05Main.class).findSingleMethod("main");
+        NodeList thrownExceptionsList = method.getThrownExceptions();
+        Assert.assertEquals("method main does not declare throws",0, thrownExceptionsList.size());
+    }
+
+    @Test
+    public void testMainContainsTwoCatchClauses() throws Exception {
+        MethodDeclaration method = new CodeParser("task05", Task05Main.class).findSingleMethod("main");
+        List<CatchClause> catchClauses = method.findAll(CatchClause.class);
+        Assert.assertEquals("method main body contains 2 catch clauses", 2, catchClauses.size());
+
+        CatchClause firstCatchClause = catchClauses.get(0);
+        Assert.assertTrue("first catch clause should be FileNotFoundException",
+                firstCatchClause.getParameter().getType().toString().contains("FileNotFoundException"));
+
+        CatchClause secondCatchClause = catchClauses.get(1);
+        Assert.assertTrue("second catch clause should be IOException",
+                secondCatchClause.getParameter().getType().toString().contains("IOException"));
+    }
+
+}
