5c0de0b2623259ce886cb3436b9240fe655f6ae3
Meghavor <picnik753@gmail.com>
2020-10-27 10:40:57 +0500

    task07[Никулин]

--- task07/src/com/example/task07/Task07Main.java
+++ task07/src/com/example/task07/Task07Main.java
@@ -12,13 +12,17 @@ public class Task07Main {
     public Processor processor;
 
     public String getExceptionType() {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+
         try {
-            processor.process(); //todo вы можете заменить реализацию этого метода для ручного дебага
-        } catch (Exception e) {
+            processor.process();
+        } catch (Throwable e) {
+            if(e instanceof RuntimeException || e instanceof Error)
+                return UNCHECKED;
+            else
+                return CHECKED;
 
         }
-        return null;
+        return NONE;
     }
 
 }
