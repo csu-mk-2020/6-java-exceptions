59d8385c04e3aecebf88f60257e99e197607ce3c
Petrov <qwe@qwe.com>
2020-11-04 14:41:41 +0500

    Task07 [Теплинский]

--- task07/src/com/example/task07/Task07Main.java
+++ task07/src/com/example/task07/Task07Main.java
@@ -7,18 +7,20 @@ public class Task07Main {
     public static final String NONE = "none";
 
     public static void main(String[] args) {
+        
     }
 
     public Processor processor;
 
     public String getExceptionType() {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
         try {
-            processor.process(); //todo вы можете заменить реализацию этого метода для ручного дебага
-        } catch (Exception e) {
-
+            processor.process();
+        } catch (Throwable e) {
+            if (e instanceof RuntimeException || e instanceof Error)
+                return "unchecked";
+            else
+                return "checked";
         }
-        return null;
+        return "none";
     }
-
 }
