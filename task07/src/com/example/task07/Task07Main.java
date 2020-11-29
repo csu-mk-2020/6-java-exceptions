7a893322d93ba8ab81b4db428b2a04525ffcb0cc
Виталий Вересников <Vitosiy21@yandex.ru>
2020-11-29 17:47:52 +0500

    Task07 [Veresnikov]

--- task07/src/com/example/task07/Task07Main.java
+++ task07/src/com/example/task07/Task07Main.java
@@ -14,11 +14,15 @@ public class Task07Main {
     public String getExceptionType() {
         //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
         try {
-            processor.process(); //todo вы можете заменить реализацию этого метода для ручного дебага
-        } catch (Exception e) {
+            processor.process();
+        } catch (Throwable e) {
+            if (e instanceof RuntimeException || e instanceof Error)
+                return "unchecked";
+            else
+                return "checked";
 
         }
-        return null;
+        return "none";
     }
 
 }
