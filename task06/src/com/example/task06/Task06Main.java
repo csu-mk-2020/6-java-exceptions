98f1446be45f61408776b1b1e7c9f94a1ec66a14
Виталий Вересников <Vitosiy21@yandex.ru>
2020-11-29 17:44:46 +0500

    Task06 [Veresnikov]

--- task06/src/com/example/task06/Task06Main.java
+++ task06/src/com/example/task06/Task06Main.java
@@ -1,16 +1,28 @@
 package com.example.task06;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 public class Task06Main {
-    public static void main(String[] args) {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
+    void fun() {
         new Task06Main().printMethodName();
-         */
+    }
+
+    public static void main(String[] args) {
+        new Task06Main().fun();
     }
 
     void printMethodName() {
-        //todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        try {
+            throw new Exception();
+        } catch (Exception e) {
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            e.printStackTrace(pw);
+            String stackTrace = sw.toString().split("\n")[2];
+            stackTrace = stackTrace.substring(0, stackTrace.indexOf('('));
+            stackTrace = stackTrace.substring(stackTrace.lastIndexOf('.') + 1);
+            System.out.print(stackTrace);
+        }
     }
-
 }
